a4669b23263edc73495a9f68bfbfcff63ef04cf6
Sweat <upsidedownplatypus@gmail.com>
2020-07-07 16:52:17 -0400

    got map api functional w realtime location

--- css2/script.js
+++ css2/script.js
@@ -1,6 +1,8 @@
 const menuIcon = document.querySelector
 ('.hamburger-menu');
 const navbar= document.querySelector('.navbar');
+var latitude = "";
+var longitude = "";
 
 menuIcon.addEventListener('click',() =>{
     navbar.classList.toggle("change");
@@ -27,7 +29,9 @@ function yelpDrink() {
                 "Authorization": "Bearer onVbl7k5cpGLZGH64zpFqN58hJoywsYCQdA1MeVe9Ef-M3hxHzaGB3ORpNWfMA7S3ux-c1tMVy5r51Qq34xLirIogtuifiIcT9RFeJzWj-23_UZctLUMbD4jqsP3XnYx",
             }
         }).then(function(response) {
-            console.log(response)
+            console.log(response);
+            clearScreen;
+            initMap;
         })
       }
 }
@@ -42,8 +46,8 @@ function yelpEat() {
         console.log("Latitude: " + position.coords.latitude + " " +
         "Longitude: " + position.coords.longitude);
         
-        var latitude = position.coords.latitude;
-        var longitude = position.coords.longitude;
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
         var queryUrl = "https://cors-anywhere.herokuapp.com/api.yelp.com/v3/businesses/search?term=restaurant&latitude=" + latitude + "&longitude=" + longitude
 
         $.ajax({
@@ -53,9 +57,24 @@ function yelpEat() {
                 "Authorization": "Bearer onVbl7k5cpGLZGH64zpFqN58hJoywsYCQdA1MeVe9Ef-M3hxHzaGB3ORpNWfMA7S3ux-c1tMVy5r51Qq34xLirIogtuifiIcT9RFeJzWj-23_UZctLUMbD4jqsP3XnYx",
             }
         }).then(function(response) {
-            console.log(response)
+            console.log(response);
+            clearScreen();
+            initMap();
         })
       }
+}
+
+function initMap() {
+    var mapURL = "https://www.google.com/maps/embed/v1/view?zoom=15&center=" + latitude + "%2C" + longitude + "&key=AIzaSyDMTbiZBhMhP9h1zIfI3PWius0RL6YRBSU";
+    var mapDiv = $('<iframe>').width("100%").height("100%").attr("src", mapURL);
+    $('.map').append(mapDiv);
+    $('.map-view').html(mapURL);
+  }
+
+
+function clearScreen() {
+    $(".home-btns").html("");    
+    
 }
 
 $(document).on("click", "#food-btn", yelpEat);
